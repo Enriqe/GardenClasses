71a6c49bbe6bf8a2ebf245c99b0045c2338d2117
Francisco Canseco <frankcanseco@MacBook-Pro-de-Francisco-5.local>
2014-12-01 13:30:50 -0600

    Pedido

--- src/gardenclasses/Pedido.java
+++ src/gardenclasses/Pedido.java
@@ -5,10 +5,45 @@
  */
 package gardenclasses;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author frankcanseco
  */
 public class Pedido {
+    int pedidoId;
+    int clienteId;
+    int estado;
+    LinkedList<Producto> productos;
+    int anchoJardin;
+    int largoJardin;
+    int Dia;
+    int Mes;
+    int Ano;
+    Pedido(int pi , int ci , int ancho , int largo, int d , int m , int a){
+        this.pedidoId = pi;
+        this.clienteId=ci;
+        this.anchoJardin = ancho;
+        this.largoJardin = largo;
+        productos = new LinkedList();
+        this.Ano = a;
+        this.Mes = m;
+        this.Dia = d;
+        this.estado = 1;
+    }
+    public void agregarProducto(Producto p)
+    {
+        productos.add(p);
+    }
     
+    public double calcularTotal()
+    {
+       double total = 0;
+       for(Producto p: productos)
+       {
+           total += p.getPrecio();
+       }
+       return total;
+    }
 }
