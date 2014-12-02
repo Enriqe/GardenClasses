766c5bda0c5761c9cc43756cfae8ef5c7f2089ab
Francisco Canseco <frankcanseco@MacBook-Pro-de-Francisco-5.local>
2014-12-01 19:07:50 -0600

    Cargar

--- src/gardenclasses/GardenClasses.java
+++ src/gardenclasses/GardenClasses.java
@@ -19,20 +19,8 @@ public class GardenClasses {
         // TODO code application logic here
 
         Sistema s = new Sistema();
-        s.clientes.push(new Cliente("Frank", "Canseco", "Cumbres", "frankcanseco",
-                            "canseco", "frank.canseco@gmail.com", 1, 88808787));
+        s.Cargar();
         s.Guardar();
-       /* Pedido p;
-        p = new Pedido(1,0,40,50,17,06,1994);
-        p.agregarProducto(new Producto(1,"planta",1.0,1,1,1));
-        String s;
-        s = p.infoPedido();
-        System.out.println(p.infoPedido());
-        System.out.println(p.getProductosPendientes());
-        p=new Pedido(s);
-        System.out.println(p.infoPedido());
-        System.out.println(p.getProductosPendientes());
-               */
     }
     
 }
