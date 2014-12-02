32b67f7ca838ef60ef631c285037050588f7b30a
EnriqueM <enrique.marr@gmail.com>
2014-12-02 11:40:18 -0600

    added login and regisration

--- src/gardenclasses/GardenClasses.java
+++ src/gardenclasses/GardenClasses.java
@@ -6,21 +6,71 @@
 
 package gardenclasses;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Enrique
  */
 public class GardenClasses {
-    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-
+        Scanner in = new Scanner(System.in);
+        Boolean login;
         Sistema s = new Sistema();
         s.Cargar();
-        s.login("frankcanseco", "canseco");
+        System.out.println("Bienvenido a Garden!");
+        System.out.println("Teclea 1 si eres usuario nuevo, y 2 si ya estas registrado");
+        String menuIn = in.next();
+
+        if(menuIn == "1"){
+            System.out.println("Registro: ");
+            System.out.println("Ingresa la siguiente infromacion para registrarte: ");
+            System.out.print("Nombre: ");
+            String nombre = in.next();
+            System.out.print("Apellido: ");
+            String apellido = in.next();
+            System.out.print("Direccion: ");
+            String direccion = in.next();
+            System.out.print("Nombre de usuario: ");
+            String usuario = in.next();
+            System.out.print("Contraseña: ");
+            String password = in.next();
+            String password2;
+            do{
+                System.out.print("Confirmar contraseña: ");
+                password2 = in.next();
+            }while(password2!=password);
+            System.out.print("Mail: ");
+            String mail = in.next();
+            System.out.print("Telefono: ");
+            String telefono = in.next();
+
+            s.agregarUsuario(nombre, apellido, direccion, usuario, password, mail, telefono);
+            System.out.println("¡Gracias " + nombre +"! Hemos terminado de registrarte");
+            menuIn = "2";
+        }
+        if(menuIn == "2"){
+            System.out.println("Ingresa tu usuario y contraseña para ingresar: ");
+            System.out.print("Usuario: ");
+            String user = in.next();
+            System.out.print("Contraseña: ");
+            String pass = in.next();
+            login = s.login(user, pass);
+            if(!login){
+                do{
+                    System.out.println("Combinacion invalida, intenta de nuevo: ");
+                    System.out.print("Usuario: ");
+                    user = in.next();
+                    System.out.print("Contraseña: ");
+                    pass = in.next();
+                    login = s.login(user, pass);
+                }while(!login);
+            }
+        }
+        
         s.Guardar();
     }
     
