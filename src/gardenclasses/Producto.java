a28584c41c2bdffc3256da67bb6440d6a5d6a144
Francisco Canseco <frankcanseco@MacBook-Pro-de-Francisco-5.local>
2014-12-01 18:31:13 -0600

    Guardar

--- src/gardenclasses/Producto.java
+++ src/gardenclasses/Producto.java
@@ -24,7 +24,20 @@ public class Producto {
         this.posicionY = y;
         this.cantidadDisponible = c;
     }
-    
+    public Producto(String s){
+        s=s.replaceAll("(\\r|\\n)", "");
+        id = Integer.parseInt(s.substring(s.indexOf("I")+1, s.indexOf("$")));
+        s=(s.substring(s.indexOf("$")));
+        nombre = (s.substring(s.indexOf("$")+1, s.indexOf("%")));
+        s=(s.substring(s.indexOf("%")));
+        precio = Double.parseDouble(s.substring(s.indexOf("%")+1, s.indexOf("X")));
+        s=(s.substring(s.indexOf("X")));
+        posicionX = Integer.parseInt(s.substring(s.indexOf("X")+1, s.indexOf("Y")));
+        s=(s.substring(s.indexOf("Y")));
+        posicionY = Integer.parseInt(s.substring(s.indexOf("Y")+1, s.indexOf("C")));
+        s=(s.substring(s.indexOf("C")));
+        cantidadDisponible = Integer.parseInt(s.substring(s.indexOf("C")+1, s.indexOf("D")));
+    }
     public int getId() {
         return id;
     }
@@ -67,5 +80,21 @@ public class Producto {
 
     public void addCantidadDisponible(int cantidad) {
         this.cantidadDisponible += cantidad;
+    }
+    public String generaInfo(){
+        String s="I";
+        s=s.concat(Integer.toString(id));
+        s=s.concat("$");
+        s=s.concat(nombre);
+        s=s.concat("%");
+        s=s.concat(Double.toString(precio));
+        s=s.concat("X");
+        s=s.concat(Integer.toString(posicionX));
+        s=s.concat("Y");
+        s=s.concat(Integer.toString(posicionY));
+        s=s.concat("C");
+        s=s.concat(Integer.toString(cantidadDisponible));
+        s=s.concat("D");
+        return s;
     }
 }
